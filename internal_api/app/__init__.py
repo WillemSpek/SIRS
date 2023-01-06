9e0d8d91898b7bcb2742da788ff56f401e4a15b3
root <root@debian>
2023-01-06 03:12:04 +0000

    Final commit

--- internal_api/app/__init__.py
+++ internal_api/app/__init__.py
@@ -5,8 +5,8 @@ from pymongo import MongoClient
 
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-# app.config['MONGO_URI'] = os.environ.get('MOGNO_URI')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['MONGO_URI'] = os.environ.get('MOGNO_URI')
 
 mongodb_client = MongoClient(app, tls=True, tlsCAFile='../certs/db_cert.pem')
 db = mongodb_client.db
